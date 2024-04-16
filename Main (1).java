ce9389cd36adeb651c0bed54faef11f752a511fd
Mattkm023 <166042902+Mattkm023@users.noreply.github.com>
2024-04-16 08:50:05 -0700

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,17 @@
+class Main{
+        public static void moon() {
+          System.out.print( "dim" );
+        }
+        public static void winter(int temp) {
+            if( temp > 50){
+                System.out.print( " * " );
+                temp -= 1;
+                winter(temp);
+            }
+        }
+        public static void main(String[] args) {
+            winter(65);
+            moon();
+      }
+}
+          
